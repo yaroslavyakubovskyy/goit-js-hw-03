9959ade639e48ba3d7b878973493a168299e266a
Yaroslav <shopogolik.raj@gmail.com>
2025-02-14 13:50:46 +0100

    make hm-3

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function filterArray(numbers, value) {
+  let editedArray = [];
+
+  for (const nuber of numbers) {
+    if (nuber > value) {
+      editedArray.push(nuber);
+    }
+  }
+
+  return editedArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
